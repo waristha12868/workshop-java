0e2d6642f14b592a6e6df665865aa45621d36780
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-02 14:15:26 +0700

    Success with fail case

--- demo01/src/test/java/unittest/HelloTest.java
+++ demo01/src/test/java/unittest/HelloTest.java
@@ -1,53 +1,52 @@
-  package unittest;
+package unittest;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserDBWithSuccess extends UserDB {
-    @Override
-    public String getNameById(int id) {
-        return "waristha";
-    }
-}
-
 class HelloTest {
 
     @Test
-    @DisplayName("ทำการทดสอบกับ database (I = Isolate/Independent)")
-    public void case02() {
+    @DisplayName("case-demo01")
+    public void case01() {
+        // Arrange
         Hello hello = new Hello();
-        hello.userDB = new UserDBWithSuccess();
+        // Act
+        String actualResult = hello.hi("waristha");
+        // Assert
+        assertEquals("Hello, waristha", actualResult);
+    }
+
+    @Test
+    @DisplayName("case-demo02 : ทดสอบ database")
+    public void case02() {
+        // Arrange
+        Hello hello = new Hello();
+        hello.userDB = new UserDB() {
+            @Override
+            public String getNameById(int id) {
+                return "waristha";
+            }
+        };
+        // Act
         String name = hello.workWithDb(1);
+        // Assert
         assertEquals("waristha", name);
     }
 
     @Test
-    @DisplayName("เกิด exception เมื่อค้นหาผู้ใช้งาน id=2 ไม่เจอ")
+    @DisplayName("กรณี exception เมื่อหาผู้ใช้งาน Id=2 ไม่เจอ")
     public void case03() {
         Hello hello = new Hello();
         hello.userDB = new UserDB(){
             @Override
             public String getNameById(int id){
-                throw new UserNotFoundException("Id=" + id + " not found");
+                throw new UserNotFoundException("Id=" + id + " Not found");
             }
         };
-        // Junit 5 + Exception
         Exception exception = assertThrows(UserNotFoundException.class, () ->
                 hello.workWithDb(2));
-        assertEquals("Id=2 not found", exception.getMessage());
+        assertEquals("Id=2 Not found", exception.getMessage());
     }
-
-    @Test
-    @DisplayName("นี่คือ case แรกของการเขียน")
-    public void case01() {
-        // Arrange == Given == Pre condition
-        Hello hello = new Hello();
-        // Act  == When == Action == Call target method
-        String actualResult = hello.hi("somkiat");
-        // Assert == Validate with expected result
-        assertEquals("Hello, somkiat", actualResult);
-    }
-
 }
