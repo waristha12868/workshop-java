d425622c0e2a58a9814c2702b145645c1e2e9f01
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-02 14:12:38 +0700

    Connect DB

--- demo01/src/main/java/unittest/Hello.java
+++ demo01/src/main/java/unittest/Hello.java
@@ -1,26 +1,20 @@
 package unittest;
 
 public class Hello {
+
+    UserDB userDB;
+
     public String hi(String name) {
         return "Hello, " + name;
     }
 
-    UserDB userDB;
-
     public String workWithDb(int id) {
-        // Tight coupling => Loose coupling
         return userDB.getNameById(id);
     }
 }
 
-class UserDB { // Connect to DB
+class UserDB {
     public String getNameById(int id) {
-        // Unstable with dependency
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        throw new RuntimeException("DB Fail");
+        throw new RuntimeException("DB fail");
     }
 }
