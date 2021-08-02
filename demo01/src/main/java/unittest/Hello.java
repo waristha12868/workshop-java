6a5b41d3e44b648e9647eebcaa0661fbaf503f7a
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-02 13:41:39 +0700

    Demo with I = isolate/independent

--- demo01/src/main/java/unittest/Hello.java
+++ demo01/src/main/java/unittest/Hello.java
@@ -1,8 +1,20 @@
 package unittest;
 
 public class Hello {
-
     public String hi(String name) {
-        return "Hello " + name;
+        return "Hello, " + name;
+    }
+
+    public String workWithDb(int id) {
+        // Tight coupling
+        UserDB userDB = new UserDB();
+        return userDB.getNameById(id);
+    }
+}
+
+class UserDB { // Connect to DB
+    public String getNameById(int id) {
+        // Unstable with dependency
+        throw new RuntimeException("DB Fail");
     }
 }
