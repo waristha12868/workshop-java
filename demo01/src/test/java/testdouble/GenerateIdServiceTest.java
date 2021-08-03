9a3b8111e66f395b05a3e3d9a0c65fae59a1b0ad
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-03 14:26:22 +0700

    add spy

--- demo01/src/test/java/testdouble/GenerateIdServiceTest.java
+++ demo01/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -1,17 +1,62 @@
-  
 package testdouble;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class GenerateIdServiceTest {
+class GenerateIdServiceTest {
+
     @Test
-    @DisplayName("ID = XYZ7")
+    @DisplayName("[DEMO STUB] ID = XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        service.setRandom(new StubRandom(9));
         String id = service.get();
-        assertEquals("XYZ7", id);
+        assertEquals("XYZ9", id);
+    }
+
+    @Test
+    @DisplayName("[DEMO SPY] Called nextInt()")
+    public void case02() {
+        GenerateIdService service = new GenerateIdService();
+        SpyRandom spyRandom = new SpyRandom();
+        service.setRandom(spyRandom);
+        service.get();
+        spyRandom.verify(1);
+    }
+
+}
+
+class SpyRandom extends Random {
+
+    private int counter = 0;
+
+    @Override
+    public int nextInt(int bound) {
+        this.counter++;
+        return 1000;
+    }
+
+    public void verify(int expectedCount) {
+        if (expectedCount != counter) {
+            fail("จำนวนการเรียกใช้งาน method nextInt() ไม่ถูกต้อง");
+        }
+    }
+}
+
+class StubRandom extends Random {
+
+    private final int number;
+
+    public StubRandom(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return number;
     }
 }
