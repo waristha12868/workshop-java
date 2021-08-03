d227cf29065d11582c8fe97373e26072b77bd3e1
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-03 16:36:40 +0700

    testing unit test + mockito

---
+++ workshop-dip/src/main/java/com/example/workshopdip/GenerateIdService.java
@@ -0,0 +1,23 @@
+package com.example.workshopdip;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class GenerateIdService {
+
+    @Autowired
+    private Random random;
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public String get() {
+        int number = random.nextInt(10);
+        String id = "XYZ" + number;
+        return id;
+    }
+}
